622fae662ae1a7b15b229ccd93824cb3644a734d
Bastien Javaux <107111678+EpicSanDev@users.noreply.github.com>
2025-10-07 12:22:16 +0200

    add

--- model/train_model.py
+++ model/train_model.py
@@ -38,6 +38,25 @@ SUPPORTED_IMAGE_EXTENSIONS = {".bmp", ".jpeg", ".jpg", ".png", ".tif", ".tiff", 
 
 
 
+DEFAULT_VALUE_LOOKUP = {
+    "ace": 11,
+    "king": 10,
+    "queen": 10,
+    "jack": 10,
+    "10": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "black": 0,
+    "red": 0,
+}
+
+
 def _normalise_prefix(value: str) -> str:
     filtered = ''.join(ch if ch.isalnum() or ch in ('-', '_') else '_' for ch in value)
     filtered = filtered.strip('_').lower()
